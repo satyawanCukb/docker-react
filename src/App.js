161657cf799348a1b88ede561c6ccb3d04d56f9d
Satyawan <cukb.it@gmail.com>
2019-05-23 11:21:07 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">
+          Welcome to Docker and react.js
+        </h1>
+        </header>
+        <p className="App-intro">
+          I was changed
+        </p>
+        
+      
+    </div>
+  );
+}
+
+export default App;
